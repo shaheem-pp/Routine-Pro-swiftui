1811c6e40aa009567ea7f084c449654384960971
Shaheem PP <shanofficial2000@gmail.com>
2024-09-05 12:44:01 -0400

    Initial Commit

---
+++ Routine Pro/Routine_ProApp.swift
@@ -0,0 +1,17 @@
+//
+//  Routine_ProApp.swift
+//  Routine Pro
+//
+//  Created by Shaheem PP on 05/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Routine_ProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Routine ProUITests/Routine_ProUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Routine_ProUITestsLaunchTests.swift
+//  Routine ProUITests
+//
+//  Created by Shaheem PP on 05/09/2024.
+//
+
+import XCTest
+
+final class Routine_ProUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
